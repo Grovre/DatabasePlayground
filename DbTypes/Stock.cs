d23a16e2b00e58c6b3cec434af641d653c709aa6
Grovre <50428844+Grovre@users.noreply.github.com>
2023-04-13 09:59:58 -0500

    Stock type

---
+++ DbTypes/Stock.cs
@@ -0,0 +1,20 @@
+﻿using DbTypes.Interfaces;
+
+namespace DbTypes;
+
+public class Stock : IIdentification
+{
+    public Guid Id { get; }
+    public string Symbol { get; }
+    public double Value { get; set; }
+
+    public Stock(string symbol, double value)
+    {
+        if (symbol.Any(c => !char.IsLetter(c) && !char.IsNumber(c)))
+            throw new ArgumentException("A symbol can only consist of letters and numbers");
+        
+        Id = Guid.NewGuid();
+        Symbol = symbol;
+        Value = value;
+    }
+}
