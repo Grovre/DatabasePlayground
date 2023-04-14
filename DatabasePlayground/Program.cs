0986581ecba221d8366b22994297ddc37d5fca94
Grovre <50428844+Grovre@users.noreply.github.com>
2023-04-13 23:30:58 -0500

    New news event for affected stocks

--- DatabasePlayground/Program.cs
+++ DatabasePlayground/Program.cs
@@ -3,12 +3,17 @@
 using DatabasePlayground;
 using DbTypes;
 
-var sim = new PeopleSim(0.02, 0, 10, TimeSpan.FromSeconds(1));
+var sim = new PeopleSim(0.02, 0, 3, TimeSpan.FromSeconds(2));
 sim.SimulationTick += (_, info) =>
 {
     var goodNews = new News("Good news!", 0.05, 0.025);
-    goodNews.ApplyToMarketValues(info.stocks, false);
-    Console.WriteLine(string.Join(", ", info.stocks.Select(s => $"{s.Symbol}: {s.Value:C3}")));
+    goodNews.StockAffectedEvent += (sender, payload) =>
+    {
+        Console.WriteLine($"Stock {payload.stock.Symbol} had a {payload.percentageChange:P} change to its value with a difference of {payload.newValue - payload.oldValue:N3}");
+    };
+    
+    goodNews.ApplyToMarketValues(info.stocks.Take(2), false); // Take 2 to reduce console printing for playing
+    Console.WriteLine(string.Join(", ", info.stocks.Select(s => $"{s.Symbol}: {s.Value}\n")));
 };
 
 var cancelTokenSrc = new CancellationTokenSource(TimeSpan.FromSeconds(60));
--- DbTypes/News.cs
+++ DbTypes/News.cs
@@ -1,9 +1,13 @@
-﻿using DbTypes.Interfaces;
+﻿using System.Net.NetworkInformation;
+using DbTypes.Interfaces;
 
 namespace DbTypes;
 
 public class News : IIdentification
 {
+    public event EventHandler<(Stock stock, double oldValue, double newValue, double percentageChange)>? 
+        StockAffectedEvent;
+
     public Guid Id { get; }
     public string Headline { get; }
     public double PublicOpinionWeight { get; }
@@ -21,11 +25,17 @@ public class News : IIdentification
     {
         foreach (var stock in stocks)
         {
-            var addedValue = PublicOpinionWeight * stock.Value +
-                PublicOpinionWeightRandomOffset * 2 * Random.Shared.NextDouble() - PublicOpinionWeightRandomOffset;
+            var oldValue = stock.Value;
+            // Adding the random offset into the public opinion weight percentage
+            var percentageChange = PublicOpinionWeight +
+                PublicOpinionWeightRandomOffset * 2d * Random.Shared.NextDouble() - PublicOpinionWeightRandomOffset; 
+            var addedValue = stock.Value * percentageChange;
             if (isPositiveToStockValues)
                 addedValue = -addedValue;
             stock.Value += addedValue;
+
+            var eventPayload = (stock, oldValue, stock.Value, percentageChange);
+            StockAffectedEvent?.Invoke(this, eventPayload);
         }
     }
 }
